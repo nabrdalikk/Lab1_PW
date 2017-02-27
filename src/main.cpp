fac4762fd70f66dec71c0535beb1fb7cf9b2cb6c
Student <student@ib.polsl.pl>
2017-02-27 13:04:29 +0100

    commit3

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,16 @@
+#include <LaborkaConfig.h>
+#ifdef USE_TRIGONOMETRY_DEGREE
 #include <trygonometria.h>
+#else
+#include <mat.h>
+#endif
 int main() 
 {
-double wynik = sin_degree(15); 
+double wynik = 0;
+double pi = 3.14;
+#ifdef USE_TRIGONOMETRY_DEGREE
+wynik = sin_degree(15); 
+#else
+wynik = sin(pi/4);
+#endif
 }
